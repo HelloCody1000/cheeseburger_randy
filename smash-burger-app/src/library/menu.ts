86784454432884ff396267cd108be0f3e064a63d
codyfry0122 <codyfry0122@gmail.com>
2025-11-28 00:28:00 -0600

    Adding rest of the menu

--- smash-burger-app/src/library/menu.ts
+++ smash-burger-app/src/library/menu.ts
@@ -4,17 +4,56 @@ import type { MenuItem } from "./types";
 export const menuItems: MenuItem[] = [
   {
     id: "1",
+    name: "CheeseBurger",
+    description: "Description of the Randy burger",
+    price: 8.69,
+    category: "burger",
+  },{
+    id: "2",
     name: "The Randy",
+    description: "Description of the Randy burger",
+    price: 10.69,
+    category: "burger",
+  },{
+    id: "3",
+    name: "J.B.C",
     description: "Description of the Randy burger",
     price: 10.69,
     category: "burger",
   },
   {
-    id: "2",
+    id: "4",
     name: "Fries",
     description: "Description of the side of fries",
-    price: 2.69,
+    price: 3.69,
     category: "side",
+  },
+  {
+    id: "5",
+    name: "Rings",
+    description: "Description of the side of fries",
+    price: 6.69,
+    category: "side",
+  },{
+    id: "6",
+    name: "Frings",
+    description: "Description of the side of fries",
+    price: 5.69,
+    category: "side",
+  },
+  {
+    id: "7",
+    name: "T.G.I Fries",
+    description: "Description of the side of fries",
+    price: 9.69,
+    category: "side",
+  },
+  {
+    id: "8",
+    name: "Fountain Drink",
+    description: "Description of the side of drinks",
+    price: 2.69,
+    category: "drink",
   },
 ];
 
